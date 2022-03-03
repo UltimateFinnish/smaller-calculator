625cebf68479b70fe2bc67467c6c2ae44966e9b2
Ultimate Finnish <ultimate.finnish@gmail.com>
2022-03-03 16:38:10 +0200

    added very basic mathematic functions.

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function add(num1, num2) {
+    return resultAdd = num1 + num2;
+};
+
+function subtract(num1, num2) {
+    return resultSubtract = num1 - num2;
+};
+
+function divide(num1, num2) {
+    return resultDivide = num1 / num2;
+};
+
+function multiply(num1, num2) {
+    return resultMultiply = num1 * num2;
+}
