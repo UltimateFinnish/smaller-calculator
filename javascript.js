c73f13c9d3e928a5d77f854bf3681fdb4b6fee29
Ultimate Finnish <ultimate.finnish@gmail.com>
2022-03-05 19:07:05 +0200

    Small progression in functions.

--- javascript.js
+++ javascript.js
@@ -13,3 +13,11 @@ function divide(num1, num2) {
 function multiply(num1, num2) {
     return resultMultiply = num1 * num2;
 }
+
+let display1 = document.querySelector('[data-num1]');
+let num1 = 0;
+
+function click0() {
+    num1 = 0;
+    display1.textContent += num1;
+}
