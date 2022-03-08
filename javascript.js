4c7524c987700a66fc81ca4fcabc094917c9938d
Ultimate Finnish <ultimate.finnish@gmail.com>
2022-03-08 16:14:33 +0200

    Added functions that made each number show up in console log when said number on screen has been pressed.

--- javascript.js
+++ javascript.js
@@ -17,7 +17,41 @@ function multiply(num1, num2) {
 let display1 = document.querySelector('[data-num1]');
 let num1 = 0;
 
-function click0() {
-    num1 = 0;
-    display1.textContent += num1;
+[...document.getElementsByClassName("allButtons")].forEach(function(item) {
+    item.addEventListener('click', function() {
+        obj[this.id]();
+    })
+})
+
+let obj = {
+    btn0: function() {
+        console.log("0");
+    },
+    btn1: function() {
+        console.log("1");
+    },
+    btn2: function() {
+        console.log("2");
+    },
+    btn3: function() {
+        console.log("3");
+    },
+    btn4: function() {
+        console.log("4");
+    },
+    btn5: function() {
+        console.log("5");
+    },
+    btn6: function() {
+        console.log("6");
+    },
+    btn7: function() {
+        console.log("7");
+    },
+    btn8: function() {
+        console.log("8");
+    },
+    btn9: function() {
+        console.log("9");
+    }
 }
